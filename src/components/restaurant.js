9cbfd01fa2045c4f6023719accfa4eb0c0397a1c
Anastasiia <asya07200@gmail.com>
2022-01-23 21:26:37 +0300

    внесены правки после ревью

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,22 +1,19 @@
 import Menu from './menu';
 import Reviews from './reviews';
 import Rate from './rate';
+import styles from './restaurant.module.css';
+import { useMemo } from 'react';
 
 export default function Restaurant({ menu, label, reviews }) {
-  const averageRate = (arr) => {
-    let result = 0;
-
-    arr.forEach((el) => {
-      result += el.rating;
-    });
-
-    return result / arr.length;
-  };
+  const averageRate = useMemo(() => {
+    const result = reviews.reduce((acc, { rating }) => acc + rating, 0);
+    return Math.round(result / reviews.length);
+  }, [reviews]);
 
   return (
     <div>
-      <p style={{ fontSize: '20px', fontWeight: 'bold' }}>{label}</p>
-      <Rate value={averageRate(reviews)} />
+      <p className={styles.label}>{label}</p>
+      <Rate value={averageRate} />
       <Menu menu={menu} />
       <Reviews reviews={reviews} />
     </div>
