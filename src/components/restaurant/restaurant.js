b6e0b480ba0f96230e9cca44ccb25a45c9d3fe9c
Pavel Ksenzhik <ksyonzhik2000@mail.ru>
2022-01-24 16:04:39 +0300

    Add PropTypes to every component. Add Decrement test. Add Reviews tests

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import PropTypes from 'prop-types';
 import Menu from '../menu';
 import Reviews from '../reviews';
 import Banner from '../banner';
@@ -9,21 +10,32 @@ const Restaurant = ({ restaurant }) => {
   const { id, name, menu, reviews } = restaurant;
 
   const averageRating = useMemo(() => {
-    const total = reviews.reduce((acc, { rating }) => acc + rating, 0);
-    return Math.round(total / reviews.length);
+	const total = reviews.reduce((acc, { rating }) => acc + rating, 0);
+	return Math.round(total / reviews.length);
   }, [reviews]);
 
   return (
-    <div>
-      <Banner heading={name}>
-        <Rate value={averageRating} />
-      </Banner>
-      <div className={styles.restaurant}>
-        <Menu key={id} menu={menu} />
-        <Reviews reviews={reviews} />
-      </div>
-    </div>
+	<div>
+	  <Banner heading={name}>
+		<Rate value={averageRating} />
+	  </Banner>
+	  <div className={styles.restaurant}>
+		<Menu key={id} menu={menu} />
+		<Reviews reviews={reviews} />
+	  </div>
+	</div>
   );
 };
+
+Restaurant.propTypes = {
+  restaurant: PropTypes.shape({
+	id: PropTypes.string.isRequired,
+	reviews: PropTypes.arrayOf(
+		PropTypes.shape({
+			rating: PropTypes.number.isRequired,
+		}).isRequired,
+	).isRequired,
+  }).isRequired,
+}
 
 export default Restaurant;
--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,19 +1,26 @@
+import PropTypes from 'prop-types';
+
 import Rate from '../../rate';
 import styles from './review.module.css';
 
 const Review = ({ user, text, rating }) => (
-  <div className={styles.review}>
-    <div className={styles.content}>
+  <div className={styles.review} data-id="review">
+    <div className={styles.content} data-id="review-content">
       <div>
         <h4 className={styles.name}>{user}</h4>
         <p className={styles.comment}>{text}</p>
       </div>
-      <div className={styles.rate}>
+      <div className={styles.rate} data-id="review-rate">
         <Rate value={rating} />
       </div>
     </div>
   </div>
 );
+
+Review.propTypes = {
+  user: PropTypes.string,
+  text: PropTypes.string,
+}
 
 Review.defaultProps = {
   user: 'Anonymous',
