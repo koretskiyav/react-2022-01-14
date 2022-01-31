58fd43e2ea40b7cccf16732c1b050e221f98e39d
Alexandr Matveev <kareroyal@gmail.com>
2022-01-31 22:31:08 +0300

    corrected ht4 genid.js

--- src/redux/middleware/genid.js
+++ src/redux/middleware/genid.js
@@ -1,8 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export default (store) => (next) => (action) => {
-  action.values.userId = uuidv4();
-  action.values.reviewId = uuidv4();
-  // или надо использовать ФП?
+  if (action.type == 'ONSUBMIT') {
+    action.values.userId = uuidv4();
+    action.values.reviewId = uuidv4();
+    // или надо использовать ФП?
+  }
   next(action);
 };
