afe49ef225f4d70372defc831cdc3a0b14671078
Alexandr Matveev <kareroyal@gmail.com>
2022-02-03 23:31:11 +0300

    ht5 task 1

--- src/redux/reducer/products.js
+++ src/redux/reducer/products.js
@@ -1,10 +1,38 @@
-import { normalizedProducts } from '../../fixtures';
+import produce from 'immer';
+import { LOAD_PRODUCTS, FAILURE, REQUEST, SUCCESS } from '../constants';
 import { arrToMap } from '../utils';
 
-export default (state = arrToMap(normalizedProducts), action) => {
-  const { type } = action;
+const initialState = {
+  entities: {},
+  loading: false,
+  loaded: false,
+  error: null,
+};
 
+export default (state = initialState, action) => {
+  const { type, restId, data, error } = action;
   switch (type) {
+    case LOAD_PRODUCTS + REQUEST:
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+        error: null,
+      };
+    case LOAD_PRODUCTS + SUCCESS:
+      return {
+        ...state,
+        entities: arrToMap(data),
+        loading: false,
+        loaded: true,
+      };
+    case LOAD_PRODUCTS + FAILURE:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        error,
+      };
     default:
       return state;
   }
