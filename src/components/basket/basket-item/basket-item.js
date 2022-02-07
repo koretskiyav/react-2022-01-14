f341370386931bd76443d0a2513308b2f4d315db
Viktor Buevich <viktor.buevich@t-systems.com>
2022-02-07 21:37:41 +0300

    add HT6

--- src/components/basket/basket-item/basket-item.js
+++ src/components/basket/basket-item/basket-item.js
@@ -1,8 +1,10 @@
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import cn from 'classnames';
 import { increment, decrement, remove } from '../../../redux/actions';
 import Button from '../../button';
 import styles from './basket-item.module.css';
+import { restaurantByProductIdSelector } from '../../../redux/selectors';
 
 function BasketItem({
   product,
@@ -11,11 +13,14 @@ function BasketItem({
   increment,
   decrement,
   remove,
+  restaurant
 }) {
   return (
     <div className={styles.basketItem}>
       <div className={styles.name}>
-        <span>{product.name}</span>
+        <Link to={`/restaurants/${restaurant.id}`}>
+          <span>{product.name}</span>
+        </Link>
       </div>
       <div className={styles.info}>
         <div className={styles.counter}>
@@ -30,10 +35,14 @@ function BasketItem({
   );
 }
 
+const mapStateToProps = (state, props) => ({
+  restaurant: restaurantByProductIdSelector(state, props.product)
+})
+
 const mapDispatchToProps = (dispatch, ownProps) => ({
   increment: () => dispatch(increment(ownProps.product.id)),
   decrement: () => dispatch(decrement(ownProps.product.id)),
   remove: () => dispatch(remove(ownProps.product.id)),
 });
 
-export default connect(null, mapDispatchToProps)(BasketItem);
+export default connect(mapStateToProps, mapDispatchToProps)(BasketItem);
